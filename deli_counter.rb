38d3864e04ca9e75ef95693510e186796f4c526d
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-26 00:32:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+
+
+def line(katz_deli)
+  if katz_deli.length >= 1
+  line = []
+  counter = 1
+  katz_deli.each do |name|
+    line.push("#{counter}. #{name}")
+    counter += 1
+  end
+  puts "The line is currently: #{line.join(" ")}"
+  else 
+  puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  line = []
+  katz_deli.each do |id|
+    line.push("Welcome, #{name}"
+end 
+
+puts line([])
+puts line(["nathan", "iqra", "ted"])
